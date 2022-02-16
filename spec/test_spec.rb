2279f21c8b6786c8858b0eb85bdbdb0c921c8a7b
Taher <ttaherhagui@gmail.com>
2022-02-16 15:35:49 +0100

    test If the response body includes correct placeholder text.

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe "Hello", type: :request do
     get "/"
     expect(response).to render_template(:index)
   end
+  it "If the response body includes correct placeholder text." do
+    get "/"
+    expect(response.body).to include("Here is a number of posts  for each username")
+  end
 end
 RSpec.describe "Users", type: :request do
   it"If a correct template was rendered" do
@@ -12,6 +16,12 @@ RSpec.describe "Users", type: :request do
     get "/users/17"
     expect(response).to render_template(:show)
   end
+  it "If the response body includes correct placeholder text." do
+    get "/users"
+    expect(response.body).to include("list of all users")
+    get "/users/17"
+    expect(response.body).to include("Here is a profile of")
+  end
 end
 RSpec.describe "Posts", type: :request do
   it "If a correct template was rendered" do
@@ -19,5 +29,11 @@ RSpec.describe "Posts", type: :request do
     expect(response).to render_template(:index)
     get "/users/17/posts/1"
     expect(response).to render_template(:show)
+  end
+  it "If the response body includes correct placeholder text." do
+    get "/users/17/posts"
+    expect(response.body).to include("Here is a list of posts and interactions for given user")
+    get "/users/17/posts/1"
+    expect(response.body).to include("Here is post")
   end
 end
