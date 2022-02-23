b1f4c1527f463c59c5355587f8d2fe523009953d
Taher <ttaherhagui@gmail.com>
2022-02-23 19:36:10 +0100

    save new post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,27 @@ class PostsController < ApplicationController
       format.html { render :new, locals: { post: @post } }
     end
   end
+
   def show
     @post = Post.find_by(id: params[:id])
+  end
+
+  def create
+    current_user = User.find_by(id: params[:current_user])
+    new_post = Post.new(user: current_user, **params.require(:post).permit(:title, :text))
+    respond_to do |format|
+      format.html do 
+        if new_post.save
+          flash[:success] = "Post saved successfully"
+        else
+          flash.now[:error] = "Error: Post could not be saved"
+        end
+      end
+    end
+    begin
+      redirect_to '/users/#{current_user.id }/posts'
+    rescue
+      redirect_to user_posts_path
+    end
   end
 end
