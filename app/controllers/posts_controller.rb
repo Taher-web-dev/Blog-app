8b2092832ca54aa5b478ee2f1bde833360c439e2
Taher <ttaherhagui@gmail.com>
2022-02-23 16:43:10 +0100

    create new action for posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,13 @@ class PostsController < ApplicationController
     @user = User.find_by(id: params[:user_id])
     @posts = @user.posts
   end
-
+  def new 
+    @user = User.find_by(id: params[:current_user])
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
+    end
+  end
   def show
     @post = Post.find_by(id: params[:id])
   end
