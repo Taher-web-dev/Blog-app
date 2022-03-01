641b100ee8beb1994261baaf6b571e998aed03d1
Taher <ttaherhagui@gmail.com>
2022-03-01 11:40:28 +0100

    fix linter.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,9 @@ class LikesController < ApplicationController
   def create
     current_post = Post.find_by(id: params[:post_id])
     user_id = params[:user_id]
-    unless current_user.likes.find_by(post_id: params[:post_id])
+    if current_user.likes.find_by(post_id: params[:post_id])
+      redirect_to "/users/#{user_id}/posts/#{params[:post_id]}", info: 'You have already liked this post'
+    else
       new_like = Like.new(user: current_user, post: current_post)
       if new_like.save
         flash[:success] = 'New like added successfully'
@@ -11,6 +13,5 @@ class LikesController < ApplicationController
         redirect_to "/users/#{user_id}/posts/#{params[:post_id]}", danger: 'We could not add new like'
       end
     end
-    redirect_to "/users/#{user_id}/posts/#{params[:post_id]}", danger: 'We could not add new like'
   end
 end
